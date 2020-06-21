708e1cae1a07b7c483b3f9d18218748f201d3f64
Typiqally <typiqally@gmail.com>
2020-06-21 11:59:32 +0200

    Add configuration file

--- Summa.Forms.Identity/Services/EmailSender.cs
+++ Summa.Forms.Identity/Services/EmailSender.cs
@@ -1,45 +1,45 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace Summa.Forms.Identity.Services
 {
-    public class EmailSender : IEmailSender
-    {
-        private readonly IConfiguration _configuration;
+	public class EmailSender : IEmailSender
+	{
+		private readonly IConfiguration _configuration;
+		private readonly NetworkCredential _credential;
+		private readonly SmtpClient _client;
 
-        public EmailSender(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+		public EmailSender(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			_credential = new NetworkCredential
+			{
+				UserName = _configuration["Mail:UserName"],
+				Password = _configuration["Mail:Password"]
+			};
+			_client = new SmtpClient(_configuration["Mail:Host"])
+			{
+				Credentials = _credential,
+				Port = Convert.ToInt32(_configuration["Mail:Port"]),
+				EnableSsl = true
+			};
+		}
 
-        public async Task SendEmailAsync(string email, string subject, string message)
-        {
-            using (var client = new SmtpClient("smtp.gmail.com"))
-            {
-                var credential = new NetworkCredential
-                {
-                    UserName = "summaforms@gmail.com",
-                    Password = "29OHA3Npr5pl"
-                };
-                
-                client.Credentials = credential;
-                client.Port = 587;
-                client.EnableSsl = true;
+		public async Task SendEmailAsync(string email, string subject, string message)
+		{
+			using var emailMessage = new MailMessage
+			{
+				To = {new MailAddress(email)},
+				From = new MailAddress(_configuration["Mail:UserName"]),
+				Subject = subject,
+				Body = message,
+				IsBodyHtml = true
+			};
 
-                using (var emailMessage = new MailMessage())
-                {
-                    emailMessage.To.Add(new MailAddress(email));
-                    emailMessage.From = new MailAddress("summaforms@gmail.com");
-                    emailMessage.Subject = subject;
-                    emailMessage.Body = message;
-                    emailMessage.IsBodyHtml = true;
-                    client.Send(emailMessage);
-                }
-            }
-
-            await Task.CompletedTask;
-        }
-    }
+			await _client.SendMailAsync(emailMessage);
+		}
+	}
 }
